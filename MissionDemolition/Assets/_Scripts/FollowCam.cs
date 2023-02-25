2da1dde25798701ef45d22f24a0e013d02cc93db
ThePerfectPixl <kylelmars@gmail.com>
2023-02-24 21:59:44 -0600

    Completed Mission Demolition

--- MissionDemolition/Assets/_Scripts/FollowCam.cs
+++ MissionDemolition/Assets/_Scripts/FollowCam.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour
 {
+    static private FollowCam S;
+
+    public enum eView {none, slingshot, castle, both};
     static public GameObject POI;
 
     [Header("Inscribed")]
     public float easing = 0.05f;
     public Vector2 minXY = Vector2.zero;
+    public GameObject viewBothGO;
 
     [Header("Dynamic")]
     public float camZ;
+    public eView nextView = eView.slingshot;
 
     void Awake()
     {
+        S = this;
         camZ = this.transform.position.z;
     }
 
@@ -34,5 +40,38 @@ public class FollowCam : MonoBehaviour
         destination.z = camZ;
         transform.position = destination;
         Camera.main.orthographicSize = destination.y + 10;
+    }
+
+    static public void SWITCH_VIEW(eView newView)
+    {
+        S.SwitchView(newView);
+    }
+
+    public void SwitchView()
+    {
+        SwitchView(eView.none);
+    }
+
+    public void SwitchView(eView newView)
+    {
+        if(newView == eView.none)
+        {
+            newView = nextView;
+        }
+        switch (newView)
+        {
+            case eView.slingshot:
+                POI = null;
+                nextView = eView.castle;
+                break;
+            case eView.castle:
+                POI = MissionDemolition.GET_CASTLE();
+                nextView = eView.both;
+                break;
+            case eView.both:
+                POI = viewBothGO;
+                nextView = eView.slingshot;
+                break;
+        }
     }
 }
--- MissionDemolition/Assets/_Scripts/Slingshot.cs
+++ MissionDemolition/Assets/_Scripts/Slingshot.cs
@@ -71,9 +71,11 @@ public class Slingshot : MonoBehaviour
             projRB.isKinematic = false;
             projRB.collisionDetectionMode = CollisionDetectionMode.Continuous;
             projRB.velocity = -mouseDelta * velocityMult;
+            FollowCam.SWITCH_VIEW(FollowCam.eView.slingshot);
             FollowCam.POI = projectile;
             Instantiate<GameObject>(projLinePrefab, projectile.transform);
             projectile = null;
+            MissionDemolition.SHOT_FIRED();
         }
     }
 }
